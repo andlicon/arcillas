daa20105f68be77a85ff82d688cf9eb3492b797a
andlicon <andresjzabalac@hotmail.com>
2023-08-16 19:12:33 -0400

    ADD: Login file added and imported

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -7,9 +7,10 @@ import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+// pages
+import Login from './pages/Login.jsx';
 
 //create your first component
 const Layout = () => {
@@ -28,6 +29,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login />} path='/login'/>
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
