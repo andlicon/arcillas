edc99ec4686f8cf81095b253663088afb8d6f88f
andlicon <andresjzabalac@hotmail.com>
2023-08-17 09:50:12 -0400

    ADD: input's var has been created

--- src/front/js/component/FormInput.jsx
+++ src/front/js/component/FormInput.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/formInput.css';
 
 const FormInput = ({ type, label, restriction, id }) => {
+  const [value, setValue] = useState('')
+
+  const onChangeHandler = ({ target }) => {
+    if (restriction && !restriction()) {
+      return null;
+    }
+
+    setValue(target.value);
+  };
+
   return (
     <div className='formInput'>
       <input
         id={id}
         type={type}
-        onChange={restriction && restriction}
-        className='formInput__input formInput__input--empty' />
+        onChange={onChangeHandler}
+        value={value}
+        className={`formInput__input${value == '' ? ' formInput__input--empty' : ''}`} />
       <label htmlFor={id} className='formInput__label'>
         {
           label
