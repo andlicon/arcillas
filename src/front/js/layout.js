0c63a3d1bbdffdf16bed2c91fcce9ca48301b4df
andlicon <andresjzabalac@hotmail.com>
2023-08-17 13:01:18 -0400

    ADD: notification customized

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -20,11 +20,22 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
-            <ToastContainer />
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
@@ -32,7 +43,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<Login />} path='/login'/>
+                        <Route element={<Login />} path='/login' />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
