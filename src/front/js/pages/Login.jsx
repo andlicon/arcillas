30aa2d31aaecde7e7567aa233c5be50afec5e7ea
andlicon <andresjzabalac@hotmail.com>
2023-08-16 20:26:58 -0400

    ADD: login structure

--- src/front/js/pages/Login.jsx
+++ src/front/js/pages/Login.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import '../../styles/login.css'
 
 const Login = () => {
     return(
-        <>
-            My amazing login
-        </>
+        <div className='container'>
+            <div className='login row'>
+                <div className='login__presentation col-7'>
+                    presentation
+                </div>
+                <div className='login__logger col-5'>
+                    <form className='login__form'>
+                        form
+                    </form>
+                    <div className='login__help'>
+                        help
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 export default Login;
