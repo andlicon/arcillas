3158cf902f5f38739686d2ae5b19336b561ed3bd
andlicon <andresjzabalac@hotmail.com>
2023-08-21 10:48:28 -0400

    ADD: ProductCreate structure

--- src/front/js/pages/ProductCreate.jsx
+++ src/front/js/pages/ProductCreate.jsx
@@ -11,7 +11,14 @@ const ProductCreate = () => {
           <BackTo to='/admin/product/list' text='Volver a la lista de productos' />
         </div>
 
-        <form>
+        <form className='productCreate__form'>
+          <div className='d-flex justify-content-end'>
+            <button className='btn' type='submit'>
+              <i className="bi bi-save-fill"></i>
+              Guardar
+            </button>
+          </div>
+          {/* informacion del producto */}
           <div className="accordion" id="accordionExample">
             <div className="accordion-item">
               <h2 className="accordion-header">
@@ -22,12 +29,36 @@ const ProductCreate = () => {
               </h2>
               <div id="collapseOne" className="accordion-collapse collapse show">
                 <div className="accordion-body">
-                  <p>
-                    name, description, usage
-                  </p>
+                  <div className='row'>
+                    <label className='col-3' htmlFor="name">Nombre</label>
+                    <input className='col-9' id='name' type="text" />
+                  </div>
+                  <div className='row'>
+                    <label className='col-3' htmlFor="description">Descripción</label>
+                    <input className='col-9' id='description' type="text" />
+                  </div>
+                  <div className='row'>
+                    <label className='col-3' htmlFor="usage">Uso</label>
+                    <input className='col-9' id='usage' type="text" />
+                  </div>
+                  <div className='row'>
+                    <label className='col-3' htmlFor="categoryId">Categoria</label>
+                    <select className='col-9' name="categoryId" id="categoryId">
+                      <option value="1">1</option>
+                      <option value="1">2</option>
+                    </select>
+                  </div>
+                  <div className='row'>
+                    <label className='col-3' htmlFor="subCategoryId">Categoria</label>
+                    <select className='col-9' name="subCategoryId" id="subCategoryId">
+                      <option value="1">1</option>
+                      <option value="1">2</option>
+                    </select>
+                  </div>
                 </div>
               </div>
             </div>
+            {/* imagenes */}
             <div className="accordion-item">
               <h2 className="accordion-header">
                 <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
@@ -37,9 +68,10 @@ const ProductCreate = () => {
               </h2>
               <div id="collapseTwo" className="accordion-collapse collapse show">
                 <div className="accordion-body">
-                  <p>
-                    image
-                  </p>
+                  <div className='row'>
+                    <label className='col-3' htmlFor="image">Imagen</label>
+                    <input className='col-9' type='file' name="image" id="image" />
+                  </div>
                 </div>
               </div>
             </div>
