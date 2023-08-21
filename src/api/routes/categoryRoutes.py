ddeda9eb39f13b342aa05753f36d02596ba326ac
andlicon <andresjzabalac@hotmail.com>
2023-08-21 13:38:18 -0400

    ADD: /categorys put route

--- src/api/routes/categoryRoutes.py
+++ src/api/routes/categoryRoutes.py
@@ -65,3 +65,52 @@ def post_category():
         return jsonify({'message': 'Ocurrio un error inesperado'}), 500
 
     return jsonify(category.serialize()), 200
+
+
+@api.route('/categorys/<int:id>', methods=['PUT'])
+@jwt_required()
+def update_category(id):
+    user = User.query.filter_by(id=get_jwt_identity()).one_or_none()
+    if user is None:
+        return jsonify({'message': 'Usuario no existe'}), 404
+    if user.role is not Role.ADMIN:
+        return jsonify({'message': 'Usuario no tiene permiso'}), 401
+    
+    if not request.is_json:
+        return jsonify({'message': 'El body debe ser un JSON valido'}), 400
+
+    category = Category.query.filter_by(id=id).one_or_none()
+    if category is None:
+        return jsonify({'message': 'La categoria no existe'}), 404
+
+    body = request.get_json()
+    name = body.get('name', None)
+    category_parent = body.get('category_parent', None)
+
+    if None in [name]:
+        return jsonify({'message': 'El body no tiene los atributos necesarios'}), 400
+    if None in [name]:
+        return jsonify({'message': 'El body no tiene los atributos necesarios'}), 400
+
+    duplicated_validation = validate_new_category(name.capitalize())
+    is_valid = duplicated_validation[0]
+    if not is_valid:
+        message = duplicated_validation[1]
+        return jsonify({'message': message}), 400
+
+    if category_parent is not None:
+        aux = Category.query.filter_by(id=category_parent).one_or_none()
+        if aux is None:
+            return jsonify({'message': 'La categoria padre no existe'}), 404
+
+    category.name = name.capitalize()
+    category.category_parent=category_parent
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e.args)
+        return jsonify({'message': 'Ocurrio un error interno'}), 500
+
+    return jsonify(category.serialize()), 200
