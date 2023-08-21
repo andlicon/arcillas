9bbeb40355e689785f94e665c0b6370ddec8a094
andlicon <andresjzabalac@hotmail.com>
2023-08-21 13:42:16 -0400

    ADD: /categorys patch route

--- src/api/routes/categoryRoutes.py
+++ src/api/routes/categoryRoutes.py
@@ -89,8 +89,6 @@ def update_category(id):
 
     if None in [name]:
         return jsonify({'message': 'El body no tiene los atributos necesarios'}), 400
-    if None in [name]:
-        return jsonify({'message': 'El body no tiene los atributos necesarios'}), 400
 
     duplicated_validation = validate_new_category(name.capitalize())
     is_valid = duplicated_validation[0]
@@ -105,6 +103,52 @@ def update_category(id):
 
     category.name = name.capitalize()
     category.category_parent=category_parent
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e.args)
+        return jsonify({'message': 'Ocurrio un error interno'}), 500
+
+    return jsonify(category.serialize()), 200
+
+
+@api.route('/categorys/<int:id>', methods=['PATCH'])
+@jwt_required()
+def patch_category(id):
+    user = User.query.filter_by(id=get_jwt_identity()).one_or_none()
+    if user is None:
+        return jsonify({'message': 'Usuario no existe'}), 404
+    if user.role is not Role.ADMIN:
+        return jsonify({'message': 'Usuario no tiene permiso'}), 401
+    
+    if not request.is_json:
+        return jsonify({'message': 'El body debe ser un JSON valido'}), 400
+
+    category = Category.query.filter_by(id=id).one_or_none()
+    if category is None:
+        return jsonify({'message': 'La categoria no existe'}), 404
+
+    body = request.get_json()
+    name = body.get('name', None)
+    category_parent = body.get('category_parent', None)
+
+    if name is not None:
+        duplicated_validation = validate_new_category(name.capitalize())
+        is_valid = duplicated_validation[0]
+        if not is_valid:
+            message = duplicated_validation[1]
+            return jsonify({'message': message}), 400
+        else:
+            category.name = name.capitalize()
+
+    if category_parent is not None:
+        aux = Category.query.filter_by(id=category_parent).one_or_none()
+        if aux is None:
+            return jsonify({'message': 'La categoria padre no existe'}), 404
+        else:
+            category.category_parent=category_parent
 
     try:
         db.session.commit()
