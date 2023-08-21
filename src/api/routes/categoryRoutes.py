d2db1b26ff9a8dbe1f3ab03c4030757a2cefb148
andlicon <andresjzabalac@hotmail.com>
2023-08-21 12:46:07 -0400

    ADD: /categorys POST route

--- src/api/routes/categoryRoutes.py
+++ src/api/routes/categoryRoutes.py
@@ -1,7 +1,11 @@
-from flask import jsonify
+from flask import jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from . import api
 from ..models import db
 from ..models.Category import Category
+from ..models.User import User
+from ..models.Role import Role
+from ..utils.duplicatedUtils import validate_new_category
 
 
 @api.route('/categorys', methods=['GET'])
@@ -20,3 +24,44 @@ def get_one_category(id):
     return jsonify(category.serialize()), 200
 
 
+@api.route('/categorys', methods=['POST'])
+@jwt_required()
+def post_category():
+    user = User.query.filter_by(id=get_jwt_identity()).one_or_none()
+    if user is None:
+        return jsonify({'message': 'Usuario no existe'}), 404
+    if user.role is not Role.ADMIN:
+        return jsonify({'message': 'El usuario no tiene permisos suficiente'}), 401
+    
+    if not request.is_json:
+        return jsonify({'message': 'El body debe ser un JSON valido'}), 400
+
+    body = request.get_json()
+    name = body.get('name', None)
+    category_parent = body.get('category_parent', None)
+
+    if None in [name]:
+        return jsonify({'message': 'El body no tiene los atributos necesarios'}), 400
+
+    duplicated_validation = validate_new_category(name.capitalize())
+    is_valid = duplicated_validation[0]
+    if not is_valid:
+        message = duplicated_validation[1]
+        return jsonify({'message': message}), 400
+
+    if category_parent is not None:
+        aux = Category.query.filter_by(id=category_parent).one_or_none()
+        if aux is None:
+            return jsonify({'message': 'La categoria padre no existe'}), 404
+
+    category = Category(name=name.capitalize(), category_parent=category_parent)
+    
+    try:
+        db.session.add(category)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e.args)
+        return jsonify({'message': 'Ocurrio un error inesperado'}), 500
+
+    return jsonify(category.serialize()), 200
