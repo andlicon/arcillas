444b9933f51c6579bfded0c5b808f0c99c2ba162
andlicon <andresjzabalac@hotmail.com>
2023-08-24 10:25:16 -0400

    ADD: result selection works

--- src/front/js/component/ProductResults.jsx
+++ src/front/js/component/ProductResults.jsx
@@ -1,36 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Context } from '../store/appContext.js';
 import '../../styles/productResults.css';
 
 const ProductResults = ({ selectHandler, selected }) => {
+  const [all, setAll] = useState(false);
   const { store } = useContext(Context);
   const { productPage, categorys, units } = store;
   const { results } = productPage;
 
-  console.log(selected);
+  const onChangeAllHandler = (event) => {
+    onChangerHandler(event);
+    setAll(!all);
+  }
 
   const onChangerHandler = ({ target }) => {
     const value = target.value;
 
-    if (value == 'all') {
+    if (value == 'all' && !all) {
       const checkboxList = document.getElementsByClassName('form-check-input');
       const idList = [];
       for (const checkbox of checkboxList) {
         if (checkbox.value == 'all') continue
-        idList.push(checkbox.value);
+        idList.push(parseInt(checkbox.value));
       }
       selectHandler(idList)
-      return null;
     }
-
-    selectHandler(value);
+    else if (value == 'all' && all) {
+      selectHandler([])
+    }
+    else {
+      selectHandler(parseInt(value));
+    }
   };
 
   return (
     <table className='results container'>
       <thead>
         <tr>
-          <td> <input className="form-check-input" type="checkbox" id="select-all" value='all' aria-label="" onChange={onChangerHandler} /> </td>
+          <td> <input className="form-check-input" type="checkbox" id="select-all" value='all' aria-label="" onChange={onChangeAllHandler} /> </td>
           <td>Imagen</td>
           <td>Nombre</td>
           <td>Descripción</td>
@@ -45,11 +52,12 @@ const ProductResults = ({ selectHandler, selected }) => {
           results && results.map((product) => {
             const category = categorys.filter((element) => element.id == product.category_id);
             const unit = units.filter((element) => element.id == product.unit_id);
+            const isChecked = selected.includes(product.id);
 
             return (
               <tr className='results__tr' key={product.id}>
                 <td>
-                  <input className="form-check-input" type="checkbox" id={"select-" + product.id} value={product.id} onChange={onChangerHandler} aria-label="" />
+                  <input className="form-check-input" type="checkbox" id={"select-" + product.id} value={product.id} onChange={onChangerHandler} checked={isChecked} aria-label="" />
                 </td>
                 <td>
                   <img src={product.image_url} alt={"imagen de " + product.name} />
