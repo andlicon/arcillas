27b6fe2028e7878ff0ac0ec75699f1a37905673f
andlicon <andresjzabalac@hotmail.com>
2023-08-28 09:26:09 -0400

    ADD: product form modularized

---
+++ src/front/js/component/ProductForm.jsx
@@ -0,0 +1,80 @@
+import React, { useContext } from 'react';
+import { Context } from '../store/appContext.js';
+import { useParams } from 'react-router-dom';
+import usePopOver from '../hooks/usePopOver.jsx';
+import useProductForm from '../hooks/useProductForm.jsx';
+import PlainInput from './PlainInput.jsx';
+import PlainSelect from './PlainSelect.jsx';
+import {
+  validateProductForm
+} from '../utils/validateUtils.js';
+import '../../styles/ProductCreate.css';
+
+const ProductForm = ({ action }) => {
+  const { store } = useContext(Context);
+  const { categorys, units } = store;
+  const { productId } = useParams();
+
+  const {
+    isLoading,
+    formProduct,
+    onChangeFormProduct,
+    createProduct
+  } = useProductForm(productId);
+
+  usePopOver();
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    if (!validateProductForm(formProduct)) return null;
+
+    if (action.toLowerCase() == 'create') createProduct();
+
+  }
+
+  return (
+    <form className='productCreate__form needs-validation' id='productCreate' noValidate onSubmit={onSubmitHandler}>
+      <div className='d-flex justify-content-end'>
+        <button className='btn' type='submit' disabled={isLoading}>
+          <i className="bi bi-save-fill"></i>
+          Guardar
+        </button>
+      </div>
+      {/* PRODUCT INFORMATION */}
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#info-collapse" aria-expanded="true" aria-controls="info-collapse">
+              <i className="bi bi-info-lg productCreate__image"></i>
+              Información del producto
+            </button>
+          </h2>
+          <div id="info-collapse" className="accordion-collapse collapse show">
+            <div className="accordion-body">
+              <PlainInput id='name' name='name' value={formProduct['name']} label='Nombre' type='text' popOver='Nombre del producto' setValues={onChangeFormProduct} invalidFeedback='Se debe especificar un nombre' required={true} />
+              <PlainInput id='description' name='description' value={formProduct['description']} label='Descripción' type='text' popOver='Descripcin del producto' setValues={onChangeFormProduct} invalidFeedback='Se debe proporcionar una descripción' required={true} />
+              <PlainInput id='usage' name='usage' value={formProduct['usage']} label='Uso' type='text' popOver='Áreas o actividades en las que se usa' setValues={onChangeFormProduct} invalidFeedback='Se debe especificar un nombre' required={true} />
+              <PlainSelect id='categoryId' name='categoryId' value={formProduct['categoryId']} label='Categoría' popOver='Categoría del producto, solo puede poseer 1' setValues={onChangeFormProduct} invalidFeedback='Se debe especificar una categoría' required={true} list_items={categorys} />
+              <PlainSelect id='unitId' name='unitId' value={formProduct['unitId']} label='Unidad' popOver='Unidad en la que se presenta el producto' setValues={onChangeFormProduct} invalidFeedback='Se debe especificar una unidad' required={true} list_items={units} />
+            </div>
+          </div>
+        </div>
+        {/* IMAGES */}
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#multimedia-collapse" aria-expanded="true" aria-controls="multimedia-collapse">
+              <i className="bi bi-images productCreate__image"></i>
+              Multimedia
+            </button>
+          </h2>
+          <div id="multimedia-collapse" className="accordion-collapse collapse show">
+            <div className="accordion-body">
+              <PlainInput id='image' name='image' value={undefined} label='Imagen' type='file' popOver='Imagen del producto' setValues={onChangeFormProduct} invalidFeedback='La imagen del producto, debe ser jpg, jpge, png' required={false} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </form >
+  )
+};
+export default ProductForm;
--- src/front/js/pages/ProductEdit.jsx
+++ src/front/js/pages/ProductEdit.jsx
@@ -1,34 +1,8 @@
-import React, { useContext } from 'react';
-import { Context } from '../store/appContext.js';
-import { useParams } from 'react-router-dom';
-import '../../styles/ProductCreate.css';
-import useProductForm from '../hooks/useProductForm.jsx';
-import usePopOver from '../hooks/usePopOver.jsx';
+import React from 'react';
+import ProductForm from '../component/ProductForm.jsx';
 import BackTo from '../component/BackTo.jsx';
-import PlainInput from '../component/PlainInput.jsx';
-import PlainSelect from '../component/PlainSelect.jsx';
-import {
-  validateProductForm
-} from '../utils/validateUtils.js';
 
 const ProductEdit = () => {
-  const { productId } = useParams();
-  const { store } = useContext(Context);
-  const { categorys, units } = store;
-
-  const {
-    isLoading,
-    formProduct,
-    onChangeFormProduct
-  } = useProductForm(productId);
-
-  usePopOver();
-
-  const onSubmitHandler = async (event) => {
-    event.preventDefault();
-    if (validateProductForm(formProduct)) console.log('a');
-  }
-
   return (
     <div className='container'>
       <div className='productCreate__header'>
@@ -36,48 +10,7 @@ const ProductEdit = () => {
         <BackTo to='/admin/product' text='Volver a la lista de productos' />
       </div>
 
-      <form className='productCreate__form needs-validation' id='productCreate' noValidate onSubmit={onSubmitHandler}>
-        <div className='d-flex justify-content-end'>
-          <button className='btn' type='submit' disabled={isLoading}>
-            <i className="bi bi-save-fill"></i>
-            Guardar
-          </button>
-        </div>
-        {/* PRODUCT INFORMATION */}
-        <div className="accordion" id="accordionExample">
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#info-collapse" aria-expanded="true" aria-controls="info-collapse">
-                <i className="bi bi-info-lg productCreate__image"></i>
-                Información del producto
-              </button>
-            </h2>
-            <div id="info-collapse" className="accordion-collapse collapse show">
-              <div className="accordion-body">
-                <PlainInput id='name' name='name' value={formProduct['name']} label='Nombre' type='text' popOver='Nombre del producto' setValues={onChangeFormProduct} invalidFeedback='Se debe especificar un nombre' required={true} />
-                <PlainInput id='description' name='description' value={formProduct['description']} label='Descripción' type='text' popOver='Descripcin del producto' setValues={onChangeFormProduct} invalidFeedback='Se debe proporcionar una descripción' required={true} />
-                <PlainInput id='usage' name='usage' value={formProduct['usage']} label='Uso' type='text' popOver='Áreas o actividades en las que se usa' setValues={onChangeFormProduct} invalidFeedback='Se debe especificar un nombre' required={true} />
-                <PlainSelect id='categoryId' name='categoryId' value={formProduct['categoryId']} label='Categoría' popOver='Categoría del producto, solo puede poseer 1' setValues={onChangeFormProduct} invalidFeedback='Se debe especificar una categoría' required={true} list_items={categorys} />
-                <PlainSelect id='unitId' name='unitId' value={formProduct['unitId']} label='Unidad' popOver='Unidad en la que se presenta el producto' setValues={onChangeFormProduct} invalidFeedback='Se debe especificar una unidad' required={true} list_items={units} />
-              </div>
-            </div>
-          </div>
-          {/* IMAGES */}
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#multimedia-collapse" aria-expanded="true" aria-controls="multimedia-collapse">
-                <i className="bi bi-images productCreate__image"></i>
-                Multimedia
-              </button>
-            </h2>
-            <div id="multimedia-collapse" className="accordion-collapse collapse show">
-              <div className="accordion-body">
-                <PlainInput id='image' name='image' value={undefined} label='Imagen' type='file' popOver='Imagen del producto' setValues={onChangeFormProduct} invalidFeedback='La imagen del producto, debe ser jpg, jpge, png' required={false} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </form >
+      <ProductForm action='edit' />
     </div >
   );
 };
