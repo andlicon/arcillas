80e0259d22817c5d4d0e3dd1a610074ac0461269
andlicon <andresjzabalac@hotmail.com>
2023-09-13 10:57:58 -0400

    ADD: now background is light

--- src/front/js/component/ProductCard.jsx
+++ src/front/js/component/ProductCard.jsx
@@ -4,9 +4,9 @@ import '../../styles/productCard.css';
 
 const ProductCard = ({ product }) => {
   return (
-    <Link className='col-12 col-sm-4 col-lg-3' to={`/product/${product.id}`}>
-      <div className="productCard">
-        <img src={product.image_url} alt={product.name} className='productCard__image' />
+    <Link className='col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3' to={`/product/${product.id}`}>
+      <div className="productCard rounded">
+        <img src={product.image_url} alt={product.name} className='productCard__image rounded' />
         <div className='productCard__content'>
           <h2 className='productCard__name'>
             {
