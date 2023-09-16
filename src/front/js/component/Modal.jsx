6706e3285aa5b6f69a79265a12c739aa8eb0e44d
andlicon <andresjzabalac@hotmail.com>
2023-09-16 19:30:13 -0400

    ADD: Quote redirect to product detail

--- src/front/js/component/Modal.jsx
+++ src/front/js/component/Modal.jsx
@@ -21,24 +21,24 @@ const Modal = ({
 
   return (
     <>
-      <button type="button" className={"btn btn-primary " + button.className} data-bs-toggle="modal" data-bs-target={"#" + id}>
-        {button.icon}
-        {button.label}
+      <button type="button" className={"btn btn-primary " + button?.className} data-bs-toggle="modal" data-bs-target={"#" + id}>
+        {button?.icon}
+        {button?.label}
       </button>
 
       <div className="modal fade" id={id} tabIndex="-1" aria-labelledby={id + "Label"} aria-hidden="true">
         <div className="modal-dialog">
           <div className="modal-content box-shadow">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id={id + "Label"}>{modal.title}</h1>
+              <h1 className="modal-title fs-5" id={id + "Label"}>{modal?.title}</h1>
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              {modal.body}
+              {modal?.body}
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">{modal.cancel}</button>
-              <button id={'delete-' + idItem} type="button" className="btn btn-primary" onClick={acceptHandler}>{modal.accept}</button>
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">{modal?.cancel}</button>
+              <button id={'delete-' + idItem} type="button" className="btn btn-primary" onClick={acceptHandler}>{modal?.accept}</button>
             </div>
           </div>
         </div>
--- src/front/js/component/Quote.jsx
+++ src/front/js/component/Quote.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { useQuote } from '../hooks/useQuote';
 import LateralCard from './LateralCard.jsx';
+// import Modal from './Modal.jsx';
 import { TO_LEFT } from '../constant/positionalConstant.js';
 import '../../styles/quote.css';
+
+// const modalButton = {
+//   icon: null,
+//   label: 'Editar',
+//   className: 'btn-warning'
+// }
+
+// const modal = {
+//   title: '¿Deseas editar la cantidad del producto a cotizar?',
+//   body: '¿Estás seguro?'
+// }
 
 const Quote = ({ product, amount }) => {
   const quote = useQuote({ product, amount });
 
   return (
     <div className='quote rounded p-2 mb-2'>
-      <LateralCard item={quote.product} img_side={TO_LEFT}>
-        <p>
-          <span className='highly'>Cantidad: </span>{quote.productAmount}
-        </p>
-      </LateralCard>
+      <Link to={`/product/` + product.id}>
+        <LateralCard item={quote.product} img_side={TO_LEFT}>
+          <p>
+            <span className='highly'>Cantidad: </span>{quote.productAmount}
+          </p>
+        </LateralCard>
+      </Link>
       <div className='quote__button'>
         <button
           className='btn btn-danger'
           onClick={() => quote.remove()}>
           Eliminar
         </button>
-        <button
+        {/* <button
           className='btn btn-warning'>
           Editar
-        </button>
+        </button> */}
+        {/* <Modal
+          button={modalButton}
+          modal={modal}
+          id={`quote${product.id}`} /> */}
       </div>
     </div>
   )
