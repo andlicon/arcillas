fb16f23033226dca7772c5c25cc5bfb8781e9b66
andlicon <andresjzabalac@hotmail.com>
2023-09-17 08:43:29 -0400

    ADD: redirect to quote user page

--- src/front/js/component/QuoteList.jsx
+++ src/front/js/component/QuoteList.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Context } from '../store/appContext';
+import { Link } from "react-router-dom";
 import Quote from './Quote.jsx';
 
 export const QuoteList = () => {
@@ -25,6 +26,9 @@ export const QuoteList = () => {
           <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body flex-column">
+          <Link className='btn btn-success w-100 p-2 mb-2' to='quote'>
+            Cotizar
+          </Link>
           {
             quoteList.map((quote) => {
               const product = quote.product;
