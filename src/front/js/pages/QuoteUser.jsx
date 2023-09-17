3811c1524db0a6a20bb9cab335dbb4c4f3459b1c
andlicon <andresjzabalac@hotmail.com>
2023-09-17 09:32:41 -0400

    ADD: Product Table expect for amountList

--- src/front/js/pages/QuoteUser.jsx
+++ src/front/js/pages/QuoteUser.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { Context } from '../store/appContext';
 import useSelected from '../hooks/useSelected.jsx';
-import ProductResults from '../component/ProductResults.jsx';
+import ProductTable from '../component/ProductTable.jsx';
 
 const QuoteUser = () => {
+  const { store } = useContext(Context);
   const { selected, selectHandler, removeItems } = useSelected(null);
+  const [productList, setProductList] = useState([]);
+  const [amountList, setAmountList] = useState([])
+
+  useEffect(() => {
+    const newProductList = store?.quoteList?.map(
+      (quote) => {
+        return quote.product
+      });
+    setProductList(() => newProductList);
+
+    const newAmountList = store?.quoteList?.map(
+      (quote) => {
+        return quote.amount
+      });
+    setAmountList(() => newAmountList)
+  }, [store.quoteList]);
 
   return (
     <div className='container'>
-      <ProductResults selectHandler={selectHandler} selected={selected} />
+      <ProductTable
+        selectHandler={selectHandler}
+        selected={selected}
+        productList={productList}
+        amountList={amountList} />
     </div>
   )
 }
