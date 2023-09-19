70682215f62d3f9163b95bdb06fa62f462141d59
andlicon <andresjzabalac@hotmail.com>
2023-09-19 12:06:32 -0400

    ADD: method POST /quote

--- src/api/routes/quoteRoutes.py
+++ src/api/routes/quoteRoutes.py
@@ -7,59 +7,55 @@ from ..models.User import User
 
 
 # POST a quote
-# @api.route('/quote', methods=['POST'])
-# def post_quote():
-#     # Validating body
-#     if not request.is_json:
-#         return jsonify({'msg': 'Body must have a json item as body'}), 400
+@api.route('/quote', methods=['POST'])
+def post_quote():
+    # Validating body
+    if not request.is_json:
+        return jsonify({'msg': 'Body must have a json item as body'}), 400
 
-#     body = request.get_json()
-#     user_id = body.get("user_id")
-#     quote_list = body.get("quote_list")
-#     if None in [user_id, quote_list]:
-#         return jsonify({'msg': 'Wrong properties'}), 400
+    body = request.get_json()
+    user_id = body.get("user_id")
+    quote_items = body.get("quote_items")
+    if None in [user_id, quote_items]:
+        return jsonify({'msg': 'Wrong properties'}), 400
 
-#     # quote List must be an array list of quotes {product_id, amount}
-#     if type(quote_list) is not list:
-#         return jsonify({'msg': 'quote_list must be a list'}), 400
+    # quote List must be an array list of quotes {product_id, amount}
+    if type(quote_items) is not list:
+        return jsonify({'msg': 'quote_items must be a list'}), 400
 
-#     user = User.query.filter_by(id=user_id).one_or_none()
-#     if user is None:
-#         return jsonify({'msg': 'User not found'}), 404
+    user = User.query.filter_by(id=user_id).one_or_none()
+    if user is None:
+        return jsonify({'msg': 'User not found'}), 404
 
-#     new_quote = Quote(user_id=user.id)
+    new_quote = Quote(user_id=user.id)
     
-#     try:
-#         db.session.add(new_quote)
-#         db.session.commit()
-#     except Exception as e:
-#         db.session.rollback()
-#         print(e)
-#         return jsonify({'msg': 'Some internal error ocurred'}), 500
+    try:
+        db.session.add(new_quote)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'msg': 'Some internal error ocurred'}), 500
     
-#     for quote_item in quote_list:
-#         amount = quote_item.get('amount')
-#         product_id = quote_item.get('product_id')
+    for item in quote_items:
+        amount = item.get('amount')
+        product_id = item.get('product_id')
         
-#         if None in [amount, product_id]:
-#             # Borrar todos la quote y todos los products
-#             return jsonify({'msg': 'Quote product has wrong properties'}), 200
+        if None in [amount, product_id]:
+            # Borrar todos la quote y todos los products
+            return jsonify({'msg': 'Quote product has wrong properties'}), 200
 
-#         product_quoted = products.insert().values(
-#             quote_id = new_quote.id,
-#             product_id = product_id,
-#             amount = amount
-#         )
+        new_item = QuoteItem(product_id=product_id, amount=amount)
+        new_quote.items.append(new_item)
 
-#         try:
-#             db.session.execute(product_quoted)
-#             db.session.commit()
-#         except Exception as e:
-#             print(e)
-#             db.session.rollback()
-#             return jsonify({'msg': 'Some internal error'}), 500
-
-#     return jsonify({'msg': 'ok'}), 200
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'msg': 'Some internal error ocurred'}), 500
+        
+    return jsonify({'msg': 'ok'}), 200
 
 
 # POST a quote
